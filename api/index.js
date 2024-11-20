de6918e4075688a38f88e34079711df8f59e3e87
durgesh100bhad <durgeshbhadauriya4@gmail.com>
2024-11-20 17:00:43 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'; 
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config(); 
 
@@ -41,8 +42,8 @@ app.listen(3000,()=>{
 //isme pehle api/user route tk  jaaenge and phir 
 //userRouter me jitni bhi routes hongi vo sb tk jaaenge 
 app.use("/api/user", userRouter); 
-
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err,req, res, next) => {
     const statusCode = err.statusCode || 500;
