a40648355582118557f4932e3a9767c724df7d67
durgesh100bhad <durgeshbhadauriya4@gmail.com>
2024-11-21 21:49:43 +0530

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,29 @@
 import {FaSearch} from 'react-icons/fa'; //npm i react-icons for getting search icon inside of search box 
-import {Link} from 'react-router-dom'; //this is so that after clicking on About->we can go to about page and so on..
+import {Link, useNavigate} from 'react-router-dom'; //this is so that after clicking on About->we can go to about page and so on..
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 
 export default function Header() {
 
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
 
     <header className='bg-slate-200 shadow-md'>
@@ -19,11 +37,21 @@ export default function Header() {
             </h1>
             </Link>
 
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type='text' placeholder='Search...'
-                    className='bg-transparent focus:outline-none w-24 sm:w-64'
+            <form 
+            onSubmit={handleSubmit}
+            className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input 
+                type='text' 
+                placeholder='Search...'
+                className='bg-transparent focus:outline-none w-24 sm:w-64'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 />
-                <FaSearch className='text-slate-600'/>
+
+
+                <button>
+                  <FaSearch className='text-slate-600' />
+                </button>
             </form>
 
 
@@ -40,7 +68,10 @@ export default function Header() {
                 <li className=' text-slate-700 hover:underline'>Sign in</li> */}
                 <Link to='/profile'>
             {currentUser ? (
-              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+              <img 
+              className='rounded-full h-7 w-7 object-cover' 
+              src={currentUser.avatar}
+               alt='profile' />
             ) : (
               <li className=' text-slate-700 hover:underline'> Sign in</li>
             )}
